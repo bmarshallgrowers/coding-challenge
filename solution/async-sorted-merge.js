fc96a3b02f35259ce451b88acc45b58ea18276c8
bmarshallgrowers <bret.marshall@growersedge.com>
2022-05-13 14:28:25 -0400

    ci

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,22 @@
 "use strict";
+const Sorter = require("../lib/sorter");
 
-// Print all entries, across all of the *async* sources, in chronological order.
+//ALGORITHM
+//1) SORT ALL SOURCES BY DATE
+//2) GET THE VALUE OF THE FIRST SOURCE
+//3) POP OFF THE VALUE FROM THE FIRST SOURCE USING AWAIT BECAUSE THIS
+//   IS A PROMISE THAT IS BEING RETURNED
+//4) FILTER OFF SOURCES THAT ARE DRAINED
+//5) REPEART 1-4 UNTIL ALL SOURCES ARE DRAINED
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const sorter = new Sorter(logSources);
+    while (sorter.ready()) {
+      const value = await sorter.asyncPopAndSort();
+      printer.print(value);
+    }
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,20 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+const LogSource = require("../lib/log-source");
+const Sorter = require("../lib/sorter");
+
+//ALGORITHM
+//1) SORT ALL SOURCES BY DATE
+//2) GET THE VALUE OF THE FIRST SOURCE
+//3) POP OFF THE VALUE FROM THE FIRST SOURCE
+//4) FILTER OFF SOURCES THAT ARE DRAINED
+//5) REPEART 1-4 UNTIL ALL SOURCES ARE DRAINED
 
 module.exports = (logSources, printer) => {
+  const sorter = new Sorter(logSources);
+  while (sorter.ready()) {
+    printer.print(sorter.popAndSort());
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
